7529268b108e463395a744cc38cd21d1ad9ecd43
rlfaquino <34952829+rlfaquino@users.noreply.github.com>
2018-02-19 22:25:38 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,25 +23,25 @@ class BoilStepWithHopDropper(StepBase):
     hop_dropper = StepProperty.Actor("Hop Dropper", description="Please select the hop dropper actor")
     hop_1 = Property.Number("Hop 1 Addition", default_value="9999", configurable=True, description="First Hop alert")
     hop_1_added = Property.Number("", default_value=None)
-    hop_1_desc = Property.Text(label="Hop 1 Description", configurable=True, default_value="Hop 1", description="Enter Hop 1 descriptions, like how much use and hop name.")
+    hop_1_desc = Property.Text(label="Hop 1 Description", configurable=True, default_value="", description="Enter Hop 1 descriptions, like how much use and hop name.")
     hop_2 = Property.Number("Hop 2 Addition", default_value="9999", configurable=True, description="Second Hop alert")
     hop_2_added = Property.Number("", default_value=None)
-    hop_2_desc = Property.Text(label="Hop 2 Description", configurable=True, default_value="Hop 2", description="Enter Hop 2 descriptions, like how much use and hop name.")
+    hop_2_desc = Property.Text(label="Hop 2 Description", configurable=True, default_value="", description="Enter Hop 2 descriptions, like how much use and hop name.")
     hop_3 = Property.Number("Hop 3 Addition", default_value="9999", configurable=True, description="Third Hop alert")
     hop_3_added = Property.Number("", default_value=None)
-    hop_3_desc = Property.Text(label="Hop 3 Description", configurable=True, default_value="Hop 3", description="Enter Hop 3 descriptions, like how much use and hop name.")
+    hop_3_desc = Property.Text(label="Hop 3 Description", configurable=True, default_value="", description="Enter Hop 3 descriptions, like how much use and hop name.")
     hop_4 = Property.Number("Hop 4 Addition", default_value="9999", configurable=True, description="Fourth Hop alert")
     hop_4_added = Property.Number("", default_value=None)
-    hop_4_desc = Property.Text(label="Hop 4 Description", configurable=True, default_value="Hop 4", description="Enter Hop 4 descriptions, like how much use and hop name.")
+    hop_4_desc = Property.Text(label="Hop 4 Description", configurable=True, default_value="", description="Enter Hop 4 descriptions, like how much use and hop name.")
     hop_5 = Property.Number("Hop 5 Addition", default_value="9999", configurable=True, description="Fives Hop alert")
     hop_5_added = Property.Number("", default_value=None)
-    hop_5_desc = Property.Text(label="Hop 5 Description", configurable=True, default_value="Hop 5", description="Enter Hop 5 descriptions, like how much use and hop name.")
+    hop_5_desc = Property.Text(label="Hop 5 Description", configurable=True, default_value="", description="Enter Hop 5 descriptions, like how much use and hop name.")
     hop_6 = Property.Number("Hop 6 Addition", default_value="9999", configurable=True, description="Sixth Hop alert")
     hop_6_added = Property.Number("", default_value=None)
-    hop_6_desc = Property.Text(label="Hop 6 Description", configurable=True, default_value="Hop 6", description="Enter Hop 6 descriptions, like how much use and hop name.")
+    hop_6_desc = Property.Text(label="Hop 6 Description", configurable=True, default_value="", description="Enter Hop 6 descriptions, like how much use and hop name.")
     hop_7 = Property.Number("Hop 7 Addition", default_value="9999", configurable=True, description="Seventh Hop alert")
     hop_7_added = Property.Number("", default_value=None)
-    hop_7_desc = Property.Text(label="Hop 7 Description", configurable=True, default_value="Hop 7", description="Enter Hop 7 descriptions, like how much use and hop name.")
+    hop_7_desc = Property.Text(label="Hop 7 Description", configurable=True, default_value="", description="Enter Hop 7 descriptions, like how much use and hop name.")
 
     def init(self):
         '''
@@ -74,7 +74,7 @@ class BoilStepWithHopDropper(StepBase):
             self.__setattr__("hop_%s_added" % number, True)
             if self.hop_dropper is not None:
                 self.actor_on(int(self.hop_dropper))
-            self.notify("Hop Alert", "Please add Hop %s" % number, timeout=None)
+            self.notify("Hop Alert", "Adding Hops #%s: %s" % (number, self.__getattribute__("hop_%s_desc" % number)), timeout=10000)
 
     def execute(self):
         '''
@@ -92,8 +92,8 @@ class BoilStepWithHopDropper(StepBase):
                 self.check_hop_timer(3, self.hop_3)
                 self.check_hop_timer(4, self.hop_4)
                 self.check_hop_timer(5, self.hop_5)
-                self.check_hop_timer(5, self.hop_6)
-                self.check_hop_timer(5, self.hop_7)
+                self.check_hop_timer(6, self.hop_6)
+                self.check_hop_timer(7, self.hop_7)
         # Check if timer finished and go to next step
         if self.is_timer_finished() == True:
             self.notify("Boil Step Completed!", "Starting the next step", timeout=None)
